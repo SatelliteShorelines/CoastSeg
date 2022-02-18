26a82b7a5757008f15504abc11926f305c429e4e
2320sharon <sharonfitzpatrick@csus.edu>
2022-02-17 17:31:31 -0800

    package restructuring

---
+++ CoastSeg/exceptions.py
@@ -0,0 +1,21 @@
+class BboxTooLargeError(Exception):
+    """BboxTooLargeError: raised when bounding box is larger than MAX_BBOX_SIZE
+    Args:
+        Exception: Inherits from the base exception class
+    """
+    def __init__(self, msg="The bounding box was too large."):
+        self.msg=msg
+        super().__init__(self.msg)
+    def __str__(self):
+        return (f"{self.msg}")
+    
+class BboxTooSmallError(Exception):
+    """BboxTooLargeError: raised when bounding box is smaller than MIN_BBOX_SIZE
+    Args:
+        Exception: Inherits from the base exception class
+    """
+    def __init__(self, msg="The bounding box was too small."):
+        self.msg=msg
+        super().__init__(self.msg)
+    def __str__(self):
+        return (f"{self.msg}")
