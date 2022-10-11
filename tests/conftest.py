0fce7e7a5569fc6cc829143e11d382015e09ca02
2320sharon <sharonfitzpatrick@csus.edu>
2022-10-10 21:34:12 -0700

    new tests, new fixtures, and new test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,50 @@ import pickle
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 @pytest.fixture
+def config_dict()->dict:
+    """returns a valid dictionary of settings"""
+    settings =  {16:{}}
+    return settings
+
+
+@pytest.fixture
+def valid_settings()->dict:
+    """returns a valid dictionary of settings"""
+    settings =  {'sat_list': ['L5', 'L7', 'L8'],
+                'collection': 'C01',
+                'dates': ['2018-12-01', '2019-03-01'],
+                'cloud_thresh': 0.5,
+                'dist_clouds': 300,
+                'output_epsg': 3857,
+                'check_detection': True,
+                'adjust_detection': False,
+                'save_figure': True,
+                'min_beach_area': 4500,
+                'buffer_size': 150,
+                'min_length_sl': 200,
+                'cloud_mask_issue': False,
+                'sand_color': 'default',
+                'pan_off': 'False',
+                'create_plot': False,
+                'max_dist_ref': 25}
+    return settings
+
+@pytest.fixture
+def valid_inputs()->dict:
+    """returns a valid dictionary of inputs"""
+    inputs =  {'dates': ['2018-12-01', '2019-03-01'],
+        'sat_list': ['S2'],
+        'sitename': 'ID02022-10-07__15_hr_42_min59sec', 
+        'filepath': 'C:\\1_USGS\\CoastSeg\\repos\\2_CoastSeg\\CoastSeg_fork\\Seg2Map\\data',
+        'roi_id': 16,
+            'polygon': [[[-124.19135332563553, 40.866019455883986],
+            [-124.19199962055943, 40.902045328874756], [-124.14451802259211, 40.90252637117114], 
+            [-124.14389745792991, 40.866499891350706], [-124.19135332563553, 40.866019455883986]]],
+            'landsat_collection': 'C01'}
+    return inputs
+
+
+@pytest.fixture
 def valid_bbox_gdf()->gpd.GeoDataFrame:
     """returns the contents of valid_bbox.geojson as a gpd.GeoDataFrame"""
     file_path=os.path.abspath(os.path.join(script_dir,'test_data','valid_bbox.geojson'))
--- tests/test_common.py
+++ tests/test_common.py
@@ -7,6 +7,71 @@ import geopandas as gpd
 from shapely import geometry
 import pytest
 
+def test_create_config_gdf(valid_rois_gdf, transect_compatible_shoreline_gdf, transect_compatible_transects_gdf):
+    # test if a gdf is created with all the rois, shorelines and transects
+    actual_gdf=common.create_config_gdf(valid_rois_gdf,transect_compatible_shoreline_gdf,transect_compatible_transects_gdf)
+    assert 'type' in actual_gdf.columns
+    assert actual_gdf[actual_gdf['type']=='transect'].empty == False
+    assert actual_gdf[actual_gdf['type']=='shoreline'].empty == False
+    assert actual_gdf[actual_gdf['type']=='roi'].empty == False
+    
+    # test if a gdf is created with all the rois, transects if shorelines is None
+    shorelines_gdf=None
+    actual_gdf=common.create_config_gdf(valid_rois_gdf,shorelines_gdf,transect_compatible_transects_gdf)
+    assert 'type' in actual_gdf.columns
+    assert actual_gdf[actual_gdf['type']=='transect'].empty == False
+    assert actual_gdf[actual_gdf['type']=='shoreline'].empty == True
+    assert actual_gdf[actual_gdf['type']=='roi'].empty == False
+    # test if a gdf is created with all the rois if  transects and shorelines is None
+    transects_gdf = None
+    actual_gdf=common.create_config_gdf(valid_rois_gdf,shorelines_gdf,transects_gdf)
+    assert 'type' in actual_gdf.columns
+    assert actual_gdf[actual_gdf['type']=='transect'].empty == True
+    assert actual_gdf[actual_gdf['type']=='shoreline'].empty == True
+    assert actual_gdf[actual_gdf['type']=='roi'].empty == False
+
+
+def test_create_config_dict(valid_inputs, valid_settings)->dict:
+    # test adding an ROI's data to an empty config dictionary
+    master_config = {}
+    actual_config = common.create_config_dict(master_config, valid_inputs, valid_settings)
+    assert isinstance(actual_config,dict)
+    expected_roi = valid_inputs['roi_id']
+    # test the roi id was added as a key to config
+    assert expected_roi in actual_config
+    # test roi id is in the config list of all roi_ids
+    assert actual_config['roi_ids'] == [expected_roi]
+    assert actual_config[expected_roi]['inputs']  == valid_inputs
+    assert actual_config[expected_roi]['settings'] == valid_settings
+    
+    # test adding another ROI's data to a non-empty config dictionary
+    master_config = {'roi_ids':[23,24],
+                        23:{
+                            'settings':[],
+                            'inputs':[]
+                        },
+                            24:{
+                            'settings':[],
+                            'inputs':[]
+                        }}
+    actual_config = common.create_config_dict(master_config, valid_inputs, valid_settings)
+    assert isinstance(actual_config,dict)
+    expected_roi = valid_inputs['roi_id']
+    # test the id was added as a key to config
+    assert expected_roi in actual_config
+    # test all roi ids are in the config list of all roi_ids
+    assert actual_config['roi_ids'] == [23,24,expected_roi]
+    assert actual_config[expected_roi]['inputs']  == valid_inputs
+    assert actual_config[expected_roi]['settings'] == valid_settings
+    # ensure the other record in config dict were not changed
+    assert actual_config[23]['inputs']  == []
+    assert actual_config[23]['settings'] == []
+    # test that all the roi ids are in config
+    assert 23 in actual_config
+    assert 24 in actual_config
+    
+    
+
 
 
 def test_gdf_to_polygon(valid_bbox_gdf,valid_rois_gdf):
