21a7b574b3a2c83892f3832cf20f7cc05508b125
2320sharon <sharonfitzpatrick@csus.edu>
2022-11-30 09:01:19 -0800

    format test_extracted_shoreline

--- tests/test_extracted_shoreline.py
+++ tests/test_extracted_shoreline.py
@@ -4,41 +4,52 @@ from coastseg import exceptions
 import geopandas as gpd
 import pyproj
 
-def test_init_invalid_inputs(valid_roi_settings,valid_shoreline_gdf,valid_settings):
+
+def test_init_invalid_inputs(valid_roi_settings, valid_shoreline_gdf, valid_settings):
     # Test initialize Extracted_Shoreline with invalid ROI id
-    invalid_roi_id=4
-    roi_id='2'
-    invalid_shoreline=None
-    empty_shoreline=gpd.GeoDataFrame()
+    invalid_roi_id = 4
+    roi_id = "2"
+    invalid_shoreline = None
+    empty_shoreline = gpd.GeoDataFrame()
     shoreline = valid_shoreline_gdf
-    invalid_roi_settings=None
-    roi_settings=valid_roi_settings
-    invalid_settings=None
-    settings=valid_settings
-    
+    invalid_roi_settings = None
+    roi_settings = valid_roi_settings
+    invalid_settings = None
+    settings = valid_settings
+
     with pytest.raises(ValueError):
-        extracted_shoreline.Extracted_Shoreline(invalid_roi_id,shoreline,roi_settings,settings)
-    
+        extracted_shoreline.Extracted_Shoreline(
+            invalid_roi_id, shoreline, roi_settings, settings
+        )
+
     # Test initialize Extracted_Shoreline with invalid shoreline
     with pytest.raises(ValueError):
-        extracted_shoreline.Extracted_Shoreline(roi_id,invalid_shoreline,roi_settings,settings)
+        extracted_shoreline.Extracted_Shoreline(
+            roi_id, invalid_shoreline, roi_settings, settings
+        )
     # Test initialize Extracted_Shoreline with empty shoreline
     with pytest.raises(ValueError):
-        extracted_shoreline.Extracted_Shoreline(roi_id,empty_shoreline,roi_settings,settings)
-        
+        extracted_shoreline.Extracted_Shoreline(
+            roi_id, empty_shoreline, roi_settings, settings
+        )
+
     # Test initialize Extracted_Shoreline with invalid roi_settings
     with pytest.raises(ValueError):
-        extracted_shoreline.Extracted_Shoreline(roi_id,shoreline,invalid_roi_settings,settings)
+        extracted_shoreline.Extracted_Shoreline(
+            roi_id, shoreline, invalid_roi_settings, settings
+        )
     # Test initialize Extracted_Shoreline with empty roi_settings
     with pytest.raises(ValueError):
-        extracted_shoreline.Extracted_Shoreline(roi_id,shoreline,{},settings)
-        
+        extracted_shoreline.Extracted_Shoreline(roi_id, shoreline, {}, settings)
+
     # Test initialize Extracted_Shoreline with invalid settings
     with pytest.raises(ValueError):
-        extracted_shoreline.Extracted_Shoreline(roi_id,shoreline,roi_settings,invalid_settings)
+        extracted_shoreline.Extracted_Shoreline(
+            roi_id, shoreline, roi_settings, invalid_settings
+        )
     # Test initialize Extracted_Shoreline with empty settings
     with pytest.raises(ValueError):
-        extracted_shoreline.Extracted_Shoreline(roi_id,shoreline,roi_settings,{})
+        extracted_shoreline.Extracted_Shoreline(roi_id, shoreline, roi_settings, {})
 
 
 # def test_init(valid_roi_settings,valid_shoreline_gdf,valid_settings):
@@ -47,7 +58,7 @@ def test_init_invalid_inputs(valid_roi_settings,valid_shoreline_gdf,valid_settin
 #     shoreline = valid_shoreline_gdf
 #     roi_settings=valid_roi_settings
 #     settings=valid_settings
-    
+
 #     actual = extracted_shoreline.Extracted_Shoreline(roi_id,shoreline,roi_settings,settings)
 #     assert isinstance(actual,extracted_shoreline.Extracted_Shoreline)
 #     assert actual.roi_id==roi_id
