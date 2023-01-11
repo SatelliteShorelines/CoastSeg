8af8afa046bc6e4ca0872fe49fe35c9e1dcc4b4b
2320sharon <sharonfitzpatrick@csus.edu>
2023-01-11 15:06:55 -0800

    rename 'open model results' to 'open results'

--- src/coastseg/models_UI.py
+++ src/coastseg/models_UI.py
@@ -129,7 +129,8 @@ class UI_Models:
             description="Select:",
             disabled=False,
         )
-        self.model_implementation.observe(self.handle_model_implementation, "value")
+        self.model_implementation.observe(
+            self.handle_model_implementation, "value")
 
         self.otsu_radio = RadioButtons(
             options=["Enabled", "Disabled"],
@@ -155,7 +156,8 @@ class UI_Models:
             description="Model Input:",
             disabled=False,
         )
-        self.model_input_dropdown.observe(self.handle_model_input_change, names="value")
+        self.model_input_dropdown.observe(
+            self.handle_model_input_change, names="value")
 
         self.model_dropdown = ipywidgets.RadioButtons(
             options=self.RGB_models,
@@ -186,9 +188,9 @@ class UI_Models:
         action_style = dict(button_color="#ae3cf0")
 
         self.run_model_button = Button(description="Run Model",
-         style=action_style,
-         icon = "fa-bolt",
-         )
+                                       style=action_style,
+                                       icon="fa-bolt",
+                                       )
         self.run_model_button.on_click(self.run_model_button_clicked)
         self.use_data_button = Button(
             description="Select Data Folder",
@@ -201,9 +203,10 @@ class UI_Models:
             style=load_style,
             icon="fa-file-image-o",
         )
-        self.use_select_images_button.on_click(self.use_select_images_button_clicked)
+        self.use_select_images_button.on_click(
+            self.use_select_images_button_clicked)
         self.open_results_button = Button(
-            description="Open Model Results",
+            description="Open Results",
             style=load_style,
             icon="folder-open-o",
         )
@@ -421,7 +424,8 @@ class UI_Models:
             )
         else:
             # path to directory containing model outputs
-            model_results_path = os.path.abspath(self.model_dict["sample_direc"])
+            model_results_path = os.path.abspath(
+                self.model_dict["sample_direc"])
             if not os.path.exists(model_results_path):
                 self.launch_error_box(
                     "File Not Found",
@@ -435,7 +439,8 @@ class UI_Models:
     def load_callback(self, filechooser: FileChooser) -> None:
         if filechooser.selected:
             sample_direc = os.path.abspath(filechooser.selected)
-            print(f"The images in the folder will be segmented :\n{sample_direc} ")
+            print(
+                f"The images in the folder will be segmented :\n{sample_direc} ")
             jpgs = glob.glob1(sample_direc + os.sep, "*jpg")
             if jpgs == []:
                 self.launch_error_box(
