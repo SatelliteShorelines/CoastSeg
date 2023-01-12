43c5d3f99260d146a2b31e147eab0793505e5bbc
Sharon Fitzpatrick <SF2309@Gmail.Com>
2023-01-11 21:00:44 -0800

    fix extract shoreline bug LineString not Iterable

--- src/coastseg/models_UI.py
+++ src/coastseg/models_UI.py
@@ -185,10 +185,11 @@ class UI_Models:
         load_style = dict(button_color="#69add1")
         action_style = dict(button_color="#ae3cf0")
 
-        self.run_model_button = Button(description="Run Model",
-         style=action_style,
-         icon = "fa-bolt",
-         )
+        self.run_model_button = Button(
+            description="Run Model",
+            style=action_style,
+            icon="fa-bolt",
+        )
         self.run_model_button.on_click(self.run_model_button_clicked)
         self.use_data_button = Button(
             description="Select Data Folder",
@@ -203,7 +204,7 @@ class UI_Models:
         )
         self.use_select_images_button.on_click(self.use_select_images_button_clicked)
         self.open_results_button = Button(
-            description="Open Model Results",
+            description="Open Results",
             style=load_style,
             icon="folder-open-o",
         )
