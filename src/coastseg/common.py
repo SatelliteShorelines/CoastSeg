9f9a6041eb69a4e5486cc81587ab006b81ea4742
Sharon Fitzpatrick <SF2309@Gmail.Com>
2023-05-25 07:06:33 -0700

    utm_band_bug fix

--- src/coastseg/common.py
+++ src/coastseg/common.py
@@ -863,7 +863,7 @@ def convert_wgs_to_utm(lon: float, lat: float) -> str:
         str: new espg code
     """
     utm_band = str((math.floor((lon + 180) / 6) % 60) + 1)
-    if len(utm_band) == "1":
+    if len(utm_band) == 1:
         utm_band = "0" + utm_band
     if lat >= 0:
         epsg_code = "326" + utm_band  # North
