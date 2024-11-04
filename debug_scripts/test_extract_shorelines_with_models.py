40fab7b086d4c012369430ff0cff2a932314c663
Sharon Fitzpatrick <SF2309@Gmail.Com>
2024-11-04 14:45:21 -0800

    update scripts to test the CoastSeg zoo workflow models

---
+++ debug_scripts/test_extract_shorelines_with_models.py
@@ -0,0 +1,144 @@
+import os
+import argparse
+
+# internal python imports
+from coastseg import zoo_model
+from transformers import TFSegformerForSemanticSegmentation
+
+# how to run this script
+# Replace parameter after -P with path to ROI's RGB directory
+# python test_models.py -P <your path here>"
+# Example
+# python test_models.py -P C:\development\doodleverse\coastseg\CoastSeg\data\ID_12_datetime06-05-23__04_16_45\jpg_files\preprocessed\RGB"
+
+# Create the parser
+parser = argparse.ArgumentParser(description="Run models on provided input directory.")
+parser.add_argument(
+    "-P",
+    "--path",
+    type=str,
+    help="Path to an ROI's RGB directory from the data directory",
+)
+args = parser.parse_args()
+
+# get input directory from the args
+input_directory = args.path
+
+# uncomment the following lines for local testing only
+CoastSeg_location = r"C:\development\doodleverse\coastseg\CoastSeg" # path to CoastSeg
+roi_name = "ID_ppy1_datetime07-19-24__10_59_31" # name of the ROI directory in CoastSeg
+input_directory = os.path.join(CoastSeg_location, "data", roi_name, "jpg_files", "preprocessed", "RGB") # this is the path to the RGB directory of the ROI
+
+def create_settings(new_settings:dict={}):
+    settings ={
+        'min_length_sl': 100,       # minimum length (m) of shoreline perimeter to be valid
+        'max_dist_ref':600,         # maximum distance (m) from reference shoreline to search for valid shorelines. This detrmines the width of the buffer around the reference shoreline  
+        'cloud_thresh': 0.5,        # threshold on maximum cloud cover (0-1). If the cloud cover is above this threshold, no shorelines will be extracted from that image
+        'dist_clouds': 100,         # distance(m) around clouds where shoreline will not be mapped
+        'min_beach_area': 100,      # minimum area (m^2) for an object to be labelled as a beach
+        'sand_color': 'default',    # 'default', 'latest', 'dark' (for grey/black sand beaches) or 'bright' (for white sand beaches)
+        "apply_cloud_mask": True,   # apply cloud mask to the imagery. If False, the cloud mask will not be applied.
+    }
+    settings.update(new_settings)
+    return settings
+
+def create_model_settings(input_directory:str,img_type:str,implementation: str="BEST"):
+    """
+    Creates a settings dictionary for model configuration.
+    Args:
+        input_directory (str): The directory containing the input images.
+        img_type (str): The type of images (e.g., 'RGB', 'NIR').
+        implementation (str, optional): The implementation type, either 'BEST' or 'ENSEMBLE'. Defaults to 'BEST'.
+    Returns:
+        dict: A dictionary containing the model settings.
+    Raises:
+        AssertionError: If the input directory does not exist.
+    """
+    model_setting = {
+                "sample_direc": None, # directory of jpgs  ex. C:/Users/username/CoastSeg/data/ID_lla12_datetime11-07-23__08_14_11/jpg_files/preprocessed/RGB/",
+                "use_GPU": "0",  # 0 or 1 0 means no GPU
+                "implementation": "BEST",  # BEST or ENSEMBLE 
+                "model_type": "segformer_RGB_4class_8190958", # model name ex. segformer_RGB_4class_8190958
+                "otsu": False, # Otsu Thresholding
+                "tta": False,  # Test Time Augmentation
+            }
+    # assrt the input directory exists
+    assert os.path.exists(input_directory), f"Input directory {input_directory} does not exist"
+    model_setting["sample_direc"] = input_directory
+    model_setting["img_type"] = img_type
+    return model_setting
+
+def test_model(img_types:list, available_models_dict:dict, input_directory:str):
+    """
+    Tests models on different types of images.
+    Parameters:
+    img_types (list): A list of image types to be tested.
+    available_models_dict (dict): A dictionary where keys are image types and values are lists of models available for those image types.
+    input_directory (str): The directory where input images are stored. Should be the RGB directory for the ROI containing the images 
+    Raises:
+    Exception: If session_name is not generated correctly.
+    Prints:
+    Various debug information including the session name, implementation type, selected image type, selected model, and sample directory.
+    """
+    for img_type_index in range(len(img_types)):
+        print(f"Running models for image input type {img_type_index}")
+        selected_img_type = img_types[img_type_index]
+        model_list = available_models_dict.get(selected_img_type)
+        if not model_list:
+            print(f"No models available for {selected_img_type}")
+            continue
+
+        for model_selected in model_list:
+            print(f"Running model {model_selected}")
+            implementation = "BEST"  # "ENSEMBLE" or "BEST"
+            session_name = (
+                model_selected
+                + "_"
+                + selected_img_type
+                + "_"
+                + implementation
+                + "_"
+                + "session"
+            )
+            if not session_name:
+                raise Exception("Something went wrong...")
+
+            print(f"session_name: {session_name}")
+            print(f"implementation: {implementation}")
+            print(f"selected_img_type: {selected_img_type}")
+            print(f"model_selected: {model_selected}")
+            print(f"sample_directory: {input_directory}")
+
+            zoo_model_instance = zoo_model.Zoo_Model()
+            # create and set settings for the model
+            model_setting = create_model_settings(input_directory, selected_img_type, implementation)
+            extract_shoreline_settings = create_settings()
+            model_setting.update(extract_shoreline_settings)
+            zoo_model_instance.set_settings(**model_setting)
+
+            # run the model and extract shorelines 
+            zoo_model_instance.run_model_and_extract_shorelines(
+                        model_setting["sample_direc"],
+                        session_name=session_name,
+                        shoreline_path="",
+                        transects_path="",
+                        shoreline_extraction_area_path = "",
+                    )
+
+available_models_dict = {
+    "RGB": [
+        "global_segformer_RGB_4class_14036903", # global segformer model
+        # "AK_segformer_RGB_4class_14037041", # AK segformer model
+    ],
+    # "MNDWI": [
+    #     "segformer_MNDWI_4class_8213443",
+    # ],
+    # "NDWI": [
+    #     "segformer_NDWI_4class_8213427",
+    # ],
+}
+
+# img_types = ["RGB"]
+img_types = ["RGB", "MNDWI", "NDWI"]
+
+test_model(img_types, available_models_dict, input_directory)
--- debug_scripts/test_models.py
+++ debug_scripts/test_models.py
@@ -5,6 +5,11 @@ import argparse
 from coastseg import zoo_model
 from transformers import TFSegformerForSemanticSegmentation
 
+# Last update 11/04/2024
+# Added new RGB segformer models to the available_models_dict
+# removed old sat models from the available_models_dict
+
+
 # how to run this script
 # Replace parameter after -P with path to ROI's RGB directory
 # python test_models.py -P <your path here>"
@@ -12,7 +17,7 @@ from transformers import TFSegformerForSemanticSegmentation
 # python test_models.py -P C:\development\doodleverse\coastseg\CoastSeg\data\ID_12_datetime06-05-23__04_16_45\jpg_files\preprocessed\RGB"
 
 # Create the parser
-parser = argparse.ArgumentParser(description="Run models on provided inpput directory.")
+parser = argparse.ArgumentParser(description="Run models on provided input directory.")
 parser.add_argument(
     "-P",
     "--path",
@@ -23,82 +28,91 @@ args = parser.parse_args()
 
 # get input directory from the args
 input_directory = args.path
-# for local testing only
-input_directory = r"C:\development\doodleverse\coastseg\CoastSeg\data\ID_12_datetime06-05-23__04_16_45\jpg_files\preprocessed\RGB"
 
-# move them into their parent directories
-parent_directory_names = [
-    "segformer_RGB_4class_8190958",
-    "sat_RGB_4class_6950472",
-    "segformer_MNDWI_4class_8213443",
-    "sat_MNDWI_4class_7352850",
-    "segformer_NDWI_4class_8213427",
-    "sat_NDWI_4class_7352859",
-]
+# uncomment the following lines for local testing only
+CoastSeg_location = r"C:\development\doodleverse\coastseg\CoastSeg" # path to CoastSeg
+roi_name = "ID_ztg2_datetime07-01-24__02_39_39" # name of the ROI directory in CoastSeg
+input_directory = os.path.join(CoastSeg_location, "data", roi_name, "jpg_files", "preprocessed", "RGB") # this is the path to the RGB directory of the ROI
+
+def test_model(img_types:list, available_models_dict:dict, input_directory:str):
+    """
+    Tests models on different types of images.
+    Parameters:
+    img_types (list): A list of image types to be tested.
+    available_models_dict (dict): A dictionary where keys are image types and values are lists of models available for those image types.
+    input_directory (str): The directory where input images are stored. Should be the RGB directory for the ROI containing the images 
+    Raises:
+    Exception: If session_name is not generated correctly.
+    Prints:
+    Various debug information including the session name, implementation type, selected image type, selected model, and sample directory.
+    """
+    for img_type_index in range(len(img_types)):
+        print(f"Running models for image input type {img_type_index}")
+        selected_img_type = img_types[img_type_index]
+        model_list = available_models_dict.get(selected_img_type)
+        if not model_list:
+            print(f"No models available for {selected_img_type}")
+            continue
+
+        for model_selected in model_list:
+            print(f"Running model {model_selected}")
+            implementation = "BEST"  # "ENSEMBLE" or "BEST"
+            session_name = (
+                model_selected
+                + "_"
+                + selected_img_type
+                + "_"
+                + implementation
+                + "_"
+                + "session"
+            )
+            if not session_name:
+                raise Exception("Something went wrong...")
+
+            print(f"session_name: {session_name}")
+            print(f"implementation: {implementation}")
+            print(f"selected_img_type: {selected_img_type}")
+            print(f"model_selected: {model_selected}")
+            print(f"sample_directory: {input_directory}")
+
+            # load the basic zoo_model settings
+            model_dict = {
+                "sample_direc": input_directory,
+                "use_GPU": "0",
+                "implementation": implementation,
+                "model_type": model_selected,
+                "otsu": False,
+                "tta": False,
+            }
+
+            zoo_model_instance = zoo_model.Zoo_Model()
+
+            zoo_model_instance.run_model(
+                img_types[img_type_index],
+                model_dict["implementation"],
+                session_name,
+                model_dict["sample_direc"],
+                model_name=model_dict["model_type"],
+                use_GPU="0",
+                use_otsu=model_dict["otsu"],
+                use_tta=model_dict["tta"],
+                percent_no_data=50.0,
+            )
+
 
 available_models_dict = {
     "RGB": [
-        "segformer_RGB_4class_8190958",
-        "sat_RGB_4class_6950472",
+        "global_segformer_RGB_4class_14036903", # global segformer model
+        "AK_segformer_RGB_4class_14037041", # AK segformer model
     ],
     "MNDWI": [
         "segformer_MNDWI_4class_8213443",
-        "sat_MNDWI_4class_7352850",
     ],
     "NDWI": [
         "segformer_NDWI_4class_8213427",
-        "sat_NDWI_4class_7352859",
     ],
 }
 
-
 img_types = ["RGB", "MNDWI", "NDWI"]
 
-for img_type_index in range(len(img_types)):
-    print(f"Running models for image input type {img_type_index}")
-    selected_img_type = img_types[img_type_index]
-    model_list = available_models_dict.get(selected_img_type)
-    for model_selected in model_list:
-        print(f"Running model {model_selected}")
-        implementation = "BEST"  # "ENSEMBLE" or "BEST"
-        session_name = (
-            model_selected
-            + "_"
-            + selected_img_type
-            + "_"
-            + implementation
-            + "_"
-            + "session"
-        )
-        if not session_name:
-            raise Exception("Something went wrong...")
-
-        print(f"session_name: {session_name}")
-        print(f"implementation: {implementation}")
-        print(f"selected_img_type: {selected_img_type}")
-        print(f"model_selected: {model_selected}")
-        print(f"sample_directory: {input_directory}")
-
-        # load the basic zoo_model settings
-        model_dict = {
-            "sample_direc": input_directory,
-            "use_GPU": "0",
-            "implementation": implementation,
-            "model_type": model_selected,
-            "otsu": False,
-            "tta": False,
-        }
-
-        zoo_model_instance = zoo_model.Zoo_Model()
-
-        zoo_model_instance.run_model(
-            img_types[img_type_index],
-            model_dict["implementation"],
-            session_name,
-            model_dict["sample_direc"],
-            model_name=model_dict["model_type"],
-            use_GPU="0",
-            use_otsu=model_dict["otsu"],
-            use_tta=model_dict["tta"],
-            percent_no_data=50.0,
-        )
+test_model(img_types, available_models_dict, input_directory)
