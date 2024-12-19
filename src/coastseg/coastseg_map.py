b77b965f29a62366e6065e2fa589b7835a289183
Sharon Fitzpatrick <SF2309@Gmail.Com>
2024-12-18 21:20:06 -0800

    remove code to automatically sort images in extract_all_shorelines

--- src/coastseg/coastseg_map.py
+++ src/coastseg/coastseg_map.py
@@ -1979,25 +1979,6 @@ class CoastSeg_Map:
             
         shoreline_extraction_area_gdf = getattr(self.shoreline_extraction_area, "gdf", None) if self.shoreline_extraction_area else None
 
-        # apply good bad classifier to the downloaded imagery
-        from coastseg.classifier import run_inference_rgb_image_classifier,get_image_classifier
-        
-        for key in roi_settings.keys():
-            data_path = os.path.join(roi_settings[key]['filepath'],roi_settings[key]['sitename'])
-            RGB_path = os.path.join(data_path,'jpg_files','preprocessed','RGB')
-            print(f"Sorting images in {RGB_path}")
-            input_path =RGB_path
-            output_path = RGB_path
-            output_csv=os.path.join(RGB_path,'classification_results.csv')
-            # model_path = os.path.join(r'C:\development\doodleverse\coastseg\CoastSeg\src\coastseg\classifier_model','best.h5')
-            model_path = get_image_classifier('rgb')
-            run_inference_rgb_image_classifier(model_path,
-                        input_path,
-                        output_path,
-                        output_csv,
-                        threshold=0.40)
-
-
         #3. get selected ROIs on map and extract shoreline for each of them
         for roi_id in tqdm(roi_ids, desc="Extracting Shorelines"):
             # Create the session for the selected ROIs
