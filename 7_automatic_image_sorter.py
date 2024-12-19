51fc332d96a103e0d76e111047d2cabb473f9845
Sharon Fitzpatrick <SF2309@Gmail.Com>
2024-12-18 20:50:17 -0800

    Add a new script 7_automatic_image_sorter.py that uses the new good/bad image sorter

---
+++ 7_automatic_image_sorter.py
@@ -0,0 +1,14 @@
+from coastseg import classifier
+
+# Disclaimer this script will not work without having installed tensorflow 2.12
+
+
+# This script will automatically sort images in a directory using the model. 
+# The bad images are moved to a subdirectory called 'bad'.
+# It is meant be used on CoastSeg/data/<ROI NAME>/jpg_files/preprocessed/RGB
+
+# select the RGB directory for an ROI from /data
+input_directory =r"C:\development\doodleverse\coastseg\CoastSeg\data\ID_azb1_datetime11-01-24__02_43_46\jpg_files\preprocessed\RGB"
+
+# run the classifier to automatically sort the images
+classifier.sort_images_with_model(input_directory,threshold=0.40)
